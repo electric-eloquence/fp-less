287882dab41bcdd253a58822cd22cedbad4c5593
e2tha-e <ee@electric-eloquence.io>
2016-12-16 09:05:45 -0500

    less~extend.js

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# fp-less
+# Less extension for Fepper
+
+### Install
+Add these tasks to `excludes/extend/custom.js`:
+
+* Under gulp task 'custom:frontend-copy'
+  * 'less:frontend-copy'
+* Under gulp task 'custom:once'
+  * 'less'
+* Under gulp task 'custom:watch'
+  * 'less:watch'
+
+Create a `source/_styles/src/less` directory and put all Less code there.
---
+++ less~extend.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const conf = global.conf;
+const gulp = require('gulp');
+const plugins = require('gulp-load-plugins')();
+const runSequence = require('run-sequence');
+
+const appDir = global.appDir;
+const utils = require(`${appDir}/core/lib/utils`);
+const utilsTask = require(`${appDir}/tasker/utils`);
+
+const cssBldDir = utils.pathResolve(conf.ui.paths.source.cssBld);
+const cssSrcDir = utils.pathResolve(conf.ui.paths.source.cssSrc);
+
+gulp.task('less', function () {
+  return gulp.src(cssSrcDir + '/less/*.less')
+    .pipe(plugins.less({
+      paths: [cssSrcDir + '/less']
+    }))
+    .pipe(gulp.dest(cssBldDir));
+});
+
+gulp.task('less:frontend-copy', function (cb) {
+  runSequence(
+    'less',
+    'patternlab:copy-styles',
+    cb
+  );
+});
+
+gulp.task('less:watch', function () {
+  gulp.watch('less/**', {cwd: cssSrcDir}, ['less']);
+});
